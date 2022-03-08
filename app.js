7127bf86993e33a1d2c1c66a8c52d20ac875ddc2
Kwon Kyoung min <lanos5019@ajou.ac.kr>
2022-03-08 19:54:48 +0900

    third commit

--- app.js
+++ app.js
@@ -6,5 +6,11 @@ var human = function(name, old, height){
 
 var kyoungmin = new human("kyoungmin", 23, 177);
 
+var kyoungmin2 = {
+    name:"kyoungmin",
+    old:23,
+    height:177
+};
 
 console.log(kyoungmin["name"]);
+
