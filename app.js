9363a44d21b2bc827f2b115bff3274a45ecbf0f2
Kwon Kyoung min <lanos5019@ajou.ac.kr>
2022-03-09 22:10:07 +0900

    3.09commit

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-var human = function(name, old, height){
+/*var human = function(name, old, height){
     this.name = name;
     this.old = old;
     this.height = height;
 }
 
-var kyoungmin = new human("kyoungmin", 23, 177);
+const kyoungmin = new human("kyoungmin", 23, 177);
 
-var kyoungmin2 = {
+const kyoungmin2 = {
     name:"kyoungmin",
     old:23,
     height:177
@@ -14,3 +14,27 @@ var kyoungmin2 = {
 
 console.log(kyoungmin["name"]);
 
+const player ={
+    calc : function(firstNumber, secondNumber, typeOfCalc){
+        if(typeOfCalc = "+"){
+            console.log(firstNumber + secondNumber);
+        }
+    }
+}
+
+player.calc(15,30,"+");
+
+const age = 23;
+
+const calc = {
+    calculateKrAge : function(ageOfForeigner){
+        return ageOfForeigner + 2;
+    }
+}
+const krAge = calc.calculateKrAge(age);
+console.log(krAge);
+*/
+
+const age = prompt("How old are you?");
+const num = "14"
+console.log(num, parseInt(num));
